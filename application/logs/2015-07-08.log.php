82bbf9a055771294e7cbdb5d73b7cf5ee283578e
irisjenn26 <jennfarnacio26@gmail.com>
2015-07-08 10:14:22 -0700

    removed folders

---
+++ application/logs/2015-07-08.log.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2015-07-08 02:35:09 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/js/jquery.js, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:09 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/js/jquery.js, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:10 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_75_e6e6e6_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:10 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_flat_75_ffffff_40x100.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:10 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_888888_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:30 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_flat_75_ffffff_40x100.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:30 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_75_e6e6e6_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:35:30 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_888888_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:51 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_888888_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:51 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_75_e6e6e6_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:51 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_flat_75_ffffff_40x100.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:53 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_454545_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:53 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_75_dadada_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:54 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_65_ffffff_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:36:54 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_454545_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:39:21 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/js/jquery.js, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:39:22 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_flat_75_ffffff_40x100.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:39:22 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-bg_glass_75_e6e6e6_1x400.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
+2015-07-08 02:39:22 +02:00 --- error: Uncaught Kohana_404_Exception: The page you requested, media/css/images/ui-icons_888888_256x240.png, could not be found. in file C:/wamp/www/toshiba/system/core/Kohana.php on line 841
