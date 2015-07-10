6a0a3dd568f1fcb58ccd434078ce1d8f10868078
deveraivan <dever.ivanjules@gmail.com>
2015-07-11 05:43:52 +0800

    basic 404 error page

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -4,26 +4,31 @@
 
         echo form::open_fieldset(array('for' => 'dash_field',
                                      'class' => ''));  
+       
         echo "<h1> Welcome</h1>";
-        echo html::anchor('/admin/supply', 'view Supply Here');
+
+        echo '<div class="total_income">
+                 <label class="total">Total Income:</label>
+                 <span id="total_income"></span>
+             </div>
+
+            <div class="monthly_income">
+                <label class="month">Monthly Income:</label>
+                <span id="monthly_income"></span>
+            </div>
+
+            <div class="yearly_income">
+                <label class="year">Yearly Income:</label>
+                <span id="yearly_income"></span><br/ >';
+
+
+        echo html::anchor('/supply', 'view Supply Here');
         echo "<br>";
-        echo html::anchor('/admin/user', 'view Users Here');
+        echo html::anchor('/user', 'view Users Here');
         echo "<br>";
-        echo html::anchor('/client/request', 'view Requests Here');  
+        echo html::anchor('/request', 'view Requests Here');  
         echo form::close_fieldset();?>
 
-  <div class="total_income">
-    <label class="total">Total Income</label>
-    <span id="total_income"></span>
-  </div>
-
-  <div class="monthly_income">
-    <label class="month">Monthly Income</label>
-    <span id="monthly_income"></span>
-  </div>
-
-  <div class="yearly_income">
-    <label class="year">Yearly Income</label>
-    <span id="yearly_income"></span>
+    
   </div>
 </div>
